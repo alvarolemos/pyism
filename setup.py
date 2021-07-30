d6407cedab8877823f7a005d63ff28583fa3745f
Álvaro Lemos <alvarolemos@gmail.com>
2021-07-30 16:14:44 -0300

    Solving installation dependency issue

--- setup.py
+++ setup.py
@@ -3,11 +3,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyism',
-    version='0.0.2',
+    version='0.0.1',
     description='A scikit-multiflow API for Tensorflow-based models implementing Incremental Sequence Models (ISM)',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     author='Álvaro Lemos',
     author_email='alvarolemos@ufmg.br',
-    install_requires=['tensorflow', 'scikit-multiflow'],
+    install_requires=[
+        'numpy==1.19.2',
+        'tensorflow==2.5.0',
+        'scikit-multiflow==0.5.3'
+    ],
 )
