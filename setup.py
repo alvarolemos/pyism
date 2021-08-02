b7ee484b8bceb28748f7da694209270ac5c73867
Álvaro Lemos <alvarolemos@gmail.com>
2021-08-01 23:42:43 -0300

    Published 0.0.2 to PyPI

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
+import pathlib
 from setuptools import setup, find_packages
+
+
+README = (pathlib.Path(__file__).parent / 'README.md').read_text()
 
 
 setup(
     name='pyism',
-    version='0.0.1',
+    version='0.0.2',
     description='A scikit-multiflow API for Tensorflow-based models implementing Incremental Sequence Models (ISM)',
+    long_description=README,
+    long_description_content_type="text/markdown",
     packages=find_packages('src'),
     package_dir={'': 'src'},
+    url="https://github.com/alvarolemos/pyism",
     author='Álvaro Lemos',
     author_email='alvarolemos@ufmg.br',
     install_requires=[
